0ef023aff8fdd1aab20ddf09e7326531ea0c729a
sandralbv76 <sandralbv76@gmail.com>
2025-04-07 00:58:32 +0000

    criação do javaScript, da função para receber o valor da conta e da fução para receber o numero de pessoas

---
+++ main.js
@@ -0,0 +1,11 @@
+let bill = 0
+let tipPercentage = 0
+let numberOfPeople = 0
+
+function receiveBillVolue {
+    bill = document.querySelector("#bill").valueAsNumber
+}
+
+function receiveNumberOfPeopleValue() {
+    numberOfPeople = document.querySelector("#people").valueAsNumber
+}
