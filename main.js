ae07d1d430da77f7e9c88e23642663c73c9801e2
sandralbv76 <sandralbv76@gmail.com>
2025-04-07 01:15:10 +0000

    inicio da lógica da porcentagem da dorjeta dos botões da porcentagem da gorjeta

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 let bill = 0
 let tipPercentage = 0
 let numberOfPeople = 0
+let buttonSelected = null
 
-function receiveBillVolue {
+function receiveBillVolue() {
     bill = document.querySelector("#bill").valueAsNumber
 }
 
 function receiveNumberOfPeopleValue() {
     numberOfPeople = document.querySelector("#people").valueAsNumber
+}
+
+function receiveTipPercentageValue(value) {
+    tipPercentage = value / 100
+
+    buttonSelected = document.querySelector('#button-${value}')
+    buttonSelected.classList.add("button-selected")
 }
